1ec2600cf22233d38d80ccf3df12b5ab08e936e5
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-03-27 23:54:24 +0700

    потихоньку двигаюсь

--- Program.cs
+++ Program.cs
@@ -12,18 +12,82 @@ namespace Laba28._03
     {
         static void Main(string[] args)
         {
-            FileStream fs = new FileStream("D:\\FullNameSerialize.xml",FileMode.OpenOrCreate, FileAccess.Write);
-            FullNameClass fnc = new FullNameClass("Ivan", "Ivanov", "Ivanovich");
+            //FileStream fs = new FileStream("D:\\FullNameSerialize.xml", FileMode.OpenOrCreate, FileAccess.Write);
+            //FullNameClass fnc = new FullNameClass("Ivan", "Ivanov", "Ivanovich");
+            //fnc.Print();
+            //fnc.Serialize(fs);
+            /*fnc = new FullNameClass("Petr", "Petrov", "Petrovich");
             fnc.Print();
-            fnc.Serialize(fs);
-            fnc = new FullNameClass("Petr", "Petrov", "Petrovich");
-            fnc.Print();
-            fs = new FileStream("D:\\FullNameSerialize.xml",FileMode.OpenOrCreate, FileAccess.Read);
+            fs = new FileStream("D:\\FullNameSerialize.xml", FileMode.OpenOrCreate, FileAccess.Read);
             fnc.Deserialize(fs);
             fnc.Print();
             string path = @"D:\laba28.03";
             search kw = new search("day", path);
             kw.result();
+            */
+            string choice;//переменная ввода
+            string path;
+            string keyword;
+
+            while (true)//менюшка, ее логика
+            {
+                
+                Console.WriteLine("\n=======================\n"); //выводы
+                Console.Write("Меню: \n" +
+                    "1) Сериализовать бинарно\n" +
+                    "2) Десериализовать бинарно\n" +
+                    "3) Сериализовать в XML\n" +
+                    "4) Десериализовать XML\n" +
+                    "5) Поиск по ключевым словам\n" +
+                    "f - ВЫХОД ИЗ ПРОГРАММЫ \n " +
+                    "Ввод: ");
+                choice = Console.ReadLine();
+                Console.Clear();
+
+                //проверка на выход
+                if (choice == "f")
+                {
+                    Console.WriteLine("\n\n\n\nВЫХОД\n\n\n\n");
+                    break;
+                }
+                switch (choice)//действия по нажатию
+                {
+                    case "1":
+                        Console.WriteLine("Введите путь к файлу: ");
+                        path = Console.ReadLine();
+                        FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                        FullNameClass fnc = new FullNameClass("Ivan", "Ivanov", "Ivanovich");
+                        fnc.Print();
+                        fnc.Serialize(fs);
+                        Console.WriteLine("Сериализовано!");
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите путь к файлу: ");
+                        path = Console.ReadLine();
+                        fnc = new FullNameClass("Petr", "Petrov", "Petrovich");
+                        fnc.Print();
+                        fs = new FileStream("D:\\FullNameSerialize.xml", FileMode.OpenOrCreate, FileAccess.Read);
+                        fnc.Deserialize(fs);
+                        fnc.Print();
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        Console.WriteLine("Введите директорию: ");
+                        path = Console.ReadLine();
+                        Console.WriteLine("Введите Ключевое слово: ");
+                        keyword = Console.ReadLine();
+                        Console.WriteLine("Файлы содержащие ключевое слово: ");
+                        search kw = new search(keyword, path);
+                        kw.result();
+                        break;
+                    default:
+                        Console.WriteLine("Введено некорректное значение");
+                        break;
+                }
+            }
         }
     }
 }
